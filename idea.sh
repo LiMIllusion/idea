a8186b33f57a5ffa4b3e4ed66ba01ee1e7edfe39
LiMIllusion <luca.cesta.lm@gmail.com>
2019-10-15 00:12:43 +0200

    Create idea.sh

---
+++ idea.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+makeTemp="comp:\n\tgcc main.c\nx: comp\n\t./a.out"
+cTemp='#include <stdio.h>\n\nint main(){\n\tprintf("Hello World");\n\treturn 0;\n}'
+
+usage()
+{
+	echo -e "usage:\n[ -o | --open ] directoryName\tOpen in VS Code your previus created directory in home/dev/\n"
+	echo -e "[ -n | --new ] directoryName\tCreate a new directory in /home/dev/ with some useful files and a little template\n"
+	echo -e "[ -l | -- lang ]\tspecify your folder language for create the correct files. Currently supported languages: c\n"
+}
+
+dirName="" 
+new= 
+lang= "plain"
+while [[ "$1" != "" ]]; do
+	case "$1" in
+		-o | --open) shift
+				new=false
+				dirName="$1"
+				;;
+		-n | --new) shift
+			new=true
+			dirName="$1"
+			;;
+		-l | --lang) shift
+			lang=$1
+			;;
+		-h | --usage) usage
+			;;
+		 *) 	usage
+			exit 1
+	esac
+	shift
+done
+
+if [[ "$new" == true ]]
+ then
+	mkdir $HOME/Dev/$dirName
+	touch $HOME/Dev/$dirName/$dirName.aedi
+	case $lang in
+		c)	echo -e $makeTemp > $HOME/Dev/$dirName/makefile;
+			echo -e "$cTemp" > $HOME/Dev/$dirName/main.c;
+			;;
+	esac
+		
+fi
+if [[ "$dirName" != "" ]]
+ then
+		code $HOME/Dev/$dirName
+fi
